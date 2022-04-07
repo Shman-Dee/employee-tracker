eea4020193a0cea2880e678999140a41c2517b8a
Andrew McInally <realfunnyestate@yahoo.com>
2022-04-07 17:54:00 -0600

    moving right along

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 const inquirer = require("inquirer");
-const cTable = require('console.table');
+const cTable = require("console.table");
 
 // require('dotenv').config();
 
@@ -20,15 +20,63 @@ const greeting = () => {
   console.log("***********************************")
   
   `);
-console.log('\n')}
+  console.log("\n");
+  
+};
 greeting();
 
-connection.connect(function(err) {
-    if (err) throw err
-    console.log("Connected as Id" + connection.threadId)
-    greeting();})
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Miss you dad! We are connected on ID " + connection.threadId);
+});
 
-const options = () => {
-  inquirer.prompt([{}])
-};
+function startPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "choice",
+        choices: [
+          "View All Employees?",
+          "View All Employee's By Roles?",
+          "View all Emplyees By Deparments",
+          "Update Employee",
+          "Add Employee?",
+          "Add Role?",
+          "Add Department?",
+        ],
+      },
+    ])
+    .then(function (val) {
+      switch (val.choice) {
+        case "View All Employees?":
+          viewAllEmployees();
+          break;
 
+        case "View All Employee's By Roles?":
+          viewAllRoles();
+          break;
+        case "View all Emplyees By Deparments":
+          viewAllDepartments();
+          break;
+
+        case "Add Employee?":
+          addEmployee();
+          break;
+
+        case "Update Employee":
+          updateEmployee();
+          break;
+
+        case "Add Role?":
+          addRole();
+          break;
+
+        case "Add Department?":
+          addDepartment();
+          break;
+      }
+    });
+}
+startPrompt();
