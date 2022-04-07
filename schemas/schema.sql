6d493cc13ae3c3f2b49e9f5535843e968baa3808
Andrew McInally <realfunnyestate@yahoo.com>
2022-04-07 17:37:11 -0600

    fixed mysequelworkbench:

--- schemas/schema.sql
+++ schemas/schema.sql
@@ -1,29 +1,35 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
-USE employee_db; 
 
--- CREATE TABLE department (
---     id INTEGER PRIMARY KEY AUTO_INCREMENT,
---     name VARCHAR(30) NOT NULL
--- );
+USE employee_db;
 
--- CREATE TABLE role (
---     id INTEGER PRIMARY KEY AUTO_INCREMENT,
---     title VARCHAR(30) NOT NULL, 
---     salary DECIMAL NOT NULL,
---     department_id INTEGER, 
---     INDEX dep_ind (department_id),
---     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
--- );
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
 
--- CREATE TABLE employee (
---     id INTEGER PRIMARY KEY AUTO_INCREMENT,
---     first_name VARCHAR(30) NOT NULL,
---     last_name VARCHAR(30) NOT NULL,
---     role_id INTEGER, 
---     INDEX role_ind (role_id),
---     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
---     manager_id INTEGER,
---     INDEX manager_ind (manager_id),
---     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(30),
+    lastName VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    INDEX manager_ind (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
 );
